9471ad922a24bc928f5b1b9cc144898e5b209ae5
SamVanDijk <sdijk4@student.avans.nl>
2015-03-31 12:07:31 +0200

    Changed test

--- functions.js
+++ functions.js
@@ -1,9 +1,74 @@
-function saveUser(err){
-	console.log("Save");
-	if(err){
-		res.send(err + user);
+module.exports = {
+	// Users
+	getUsers: function(User, req, res) {
+		User.find(function(err, result){
+			res.json(result); // Returns all users
+		});
+	},
+	getUser: function(User, req, res) {
+		console.log("getUser");
+		User.findOne({UserName:req.params.UserName} ,function(err, user){
+			res.send(user); 
+		});
+	},
+	postUser: function(User, req, res) {
+		console.log('new user');
+		var user = new User();
+		user.UserName = req.body.UserName;
+		user.set('password', req.body.password);
+		console.log(req.body.DisplayName);
+		if(req.body.DisplayName == undefined)
+		{
+			user.DisplayName = user.UserName;
+		} else {
+			user.DisplayName = req.body.DisplayName;
+		}
+		if(req.body.Radius != undefined){
+			user.RadiusM = req.body.Radius;
+		}
+		if(req.body.Role != undefined){ // might wanna remove this because of security issues.
+			user.Role = req.body.Role;
+		}
+
+		console.log(user);
+		console.log(user.DisplayName);
+		
+		user.save(function(err){
+			if(err){
+				res.send(err + user);
+			}
+			else{
+				res.send(user);
+			}
+		});
+	},
+	putUser: function(User, req, res) {
+		console.log("putUser");
+		User.findOne({UserName:req.params.UserName} ,function(err, user){
+			if(req.body.DisplayName != undefined){
+				user.DisplayName = req.body.DisplayName;
+			}
+			if(req.body.password != undefined || req.body.password != ""){
+				user.password = req.body.password;
+			}
+			if(req.body.RadiusM != undefined ){
+				user.RadiusM = req.body.RadiusM;
+			}
+			if(req.body.Role != undefined){
+				user.Role = req.body.Role;
+			}
+			user.save(function(err){
+				if(!err) { res.send(user); } else { console.log(err); }
+			});
+		});
+	},
+	deleteUser: function(User, req, res) {
+		console.log("deleteUser");
+		User.findOne({UserName:req.params.UserName} ,function(err, user){
+			user.remove(function(err){
+				if(!err) { res.send(''); } else { console.log(err); }
+			});
+		});
 	}
-	else{
-		res.send(user);
-	}
-}
+	// Lines
+};
--- routes/users.js
+++ routes/users.js
@@ -4,93 +4,58 @@ var _ = require('underscore');
 var handleError;
 
 var mongoose = require('mongoose');
-// var User = mongoose.model('User');
+var User = mongoose.model('User');
 
-require('../functions.js');
+var functions = require('../functions.js');
 
 // Routing
 router.route('/')
 	.get(function(req, res, next){
-		User.find(function(err, result){
-			res.json(result); // Returns all users
-		});
+		// User.find(function(err, result){
+		// 	res.json(result); // Returns all users
+		// });
+		functions.getUsers(User, req, res);
 	})
 	.post(function(req, res, next){ //register new user
-		var User = mongoose.model('User');
-		console.log('new user');
-		var user = new User();
-		user.UserName = req.body.UserName;
-		user.set('password', req.body.password);
-		console.log(req.body.DisplayName);
-		if(req.body.DisplayName == undefined)
-		{
-			user.DisplayName = user.UserName;
-		} else {
-			user.DisplayName = req.body.DisplayName;
-		}
-		if(req.body.Radius != undefined){
-			user.RadiusM = req.body.Radius;
-		}
-		if(req.body.Role != undefined){ // might wanna remove this because of security issues.
-			user.Role = req.body.Role;
-		}
+		// var User = mongoose.model('User');
+		// console.log('new user');
+		// var user = new User();
+		// user.UserName = req.body.UserName;
+		// user.set('password', req.body.password);
+		// console.log(req.body.DisplayName);
+		// if(req.body.DisplayName == undefined)
+		// {
+		// 	user.DisplayName = user.UserName;
+		// } else {
+		// 	user.DisplayName = req.body.DisplayName;
+		// }
+		// if(req.body.Radius != undefined){
+		// 	user.RadiusM = req.body.Radius;
+		// }
+		// if(req.body.Role != undefined){ // might wanna remove this because of security issues.
+		// 	user.Role = req.body.Role;
+		// }
 
-		console.log(user);
-		console.log(user.DisplayName);
+		// console.log(user);
+		// console.log(user.DisplayName);
 		
-		// user.save(saveUser);
-		user.save(function(err){
-			if(err){
-				res.send(err + user);
-			}
-			else{
-				res.send(user);
-			}
-		});
+		// user.save(function(err){
+		// 	if(err){
+		// 		res.send(err + user);
+		// 	}
+		// 	else{
+		// 		res.send(user);
+		// 	}
+		// });
+
+		functions.postUser(User, req, res);
 	});
 	
 
 router.route('/:UserName')
-	.get(function(req, res, next){
-		User.findOne({UserName:req.params.UserName} ,function(err, user){
-			res.send(user); 
-		});
-	})
-	.put(function(req, res, next){
-		User.findOne({UserName:req.params.UserName} ,function(err, user){
-			if(req.body.DisplayName != undefined){
-				user.DisplayName = req.body.DisplayName;
-			}
-			if(req.body.password != undefined || req.body.password != ""){
-				user.password = req.body.password;
-			}
-			if(req.body.RadiusM != undefined ){
-				user.RadiusM = req.body.RadiusM;
-			}
-			if(req.body.Role != undefined){
-				user.Role = req.body.Role;
-			}
-			user.save(function(err){
-				if(!err){
-					res.send(user);
-				} else {
-					console.log(err);
-				}
-
-			});
-		});
-	})
-	.delete(function(req, res,next){
-		User.findOne({UserName:req.params.UserName} ,function(err, user){
-			user.remove(function(err){
-				if(!err) {
-					res.send('');
-				} else {
-					console.log(err);
-				}
-			});
-		});
-	});;
+	.get(function(req, res, next) { functions.getUser(User, req, res); })
+	.put(function(req, res, next) { functions.putUser(User, req, res); })
+	.delete(function(req, res,next) { functions.deleteUser(User, req, res); });
 
 
 // Export
